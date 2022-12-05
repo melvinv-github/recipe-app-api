ef1de2cddd1d3d21ffb699fd1279c1ec33d1eadb
Melvin Villas <villasmelvin@gmail.com>
2022-12-05 21:34:49 +0800

    Upload image endpoint
    Formatting changes as well, IDE now uses black

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -2,13 +2,14 @@
 Test for models.
 """
 from decimal import Decimal
+from unittest.mock import patch
 
 from core import models
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
 
-def create_user(email='user@example.com', password='testpass123'):
+def create_user(email="user@example.com", password="testpass123"):
     """Create and return a user."""
     user = get_user_model().objects.create_user(email, password)
     return user
@@ -19,8 +20,8 @@ class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
         """Test creating a user with an email is successful."""
-        email = 'test@example.com'
-        password = 'testpass123'
+        email = "test@example.com"
+        password = "testpass123"
         user = get_user_model().objects.create_user(
             email=email,
             password=password,
@@ -32,25 +33,24 @@ class ModelTests(TestCase):
     def test_new_user_email_normalized(self):
         """Test email is normalized for new users."""
         sample_emails = [
-            ['test1@EXAMPLE.COM', 'test1@example.com'],
-            ['Test2@Example.com', 'Test2@example.com'],
-            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
-            ['test4@example.COM', 'test4@example.com'],
+            ["test1@EXAMPLE.COM", "test1@example.com"],
+            ["Test2@Example.com", "Test2@example.com"],
+            ["TEST3@EXAMPLE.COM", "TEST3@example.com"],
+            ["test4@example.COM", "test4@example.com"],
         ]
 
         for email, expected in sample_emails:
-            user = get_user_model().objects.create_user(email, 'sample123')
+            user = get_user_model().objects.create_user(email, "sample123")
             self.assertEqual(user.email, expected)
 
     def test_new_user_without_email_raises_error(self):
         """Test that creating a user without an email raises a ValueError."""
         with self.assertRaises(ValueError):
-            get_user_model().objects.create_user('', 'Test123')
+            get_user_model().objects.create_user("", "Test123")
 
     def test_create_superuser(self):
         """Test creating a superuser."""
-        user = get_user_model().objects.create_superuser(
-            'test@example.com', 'test123')
+        user = get_user_model().objects.create_superuser("test@example.com", "test123")
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
@@ -58,15 +58,15 @@ class ModelTests(TestCase):
     def test_create_recipe(self):
         """Test creating a recipe is successful."""
         user = get_user_model().objects.create_user(
-            'test@example.com',
-            'testpass123',
+            "test@example.com",
+            "testpass123",
         )
         recipe = models.Recipe.objects.create(
             user=user,
-            title='Sample recipe name',
+            title="Sample recipe name",
             time_minutes=5,
-            price=Decimal('5.50'),
-            description='Sample recipe description',
+            price=Decimal("5.50"),
+            description="Sample recipe description",
         )
 
         self.assertEqual(str(recipe), recipe.title)
@@ -74,16 +74,22 @@ class ModelTests(TestCase):
     def test_create_tag(self):
         """Test creating a tag is successful."""
         user = create_user()
-        tag = models.Tag.objects.create(user=user, name='Tag1')
+        tag = models.Tag.objects.create(user=user, name="Tag1")
 
         self.assertEqual(str(tag), tag.name)
 
     def test_create_ingredient(self):
         """Test creating an ingredient is successful."""
         user = create_user()
-        ingredient = models.Ingredient.objects.create(
-            user=user,
-            name='Ingredient1'
-        )
+        ingredient = models.Ingredient.objects.create(user=user, name="Ingredient1")
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    @patch("core.models.uuid.uuid4")
+    def test_recipe_file_name_uuid(self, mock_uuid):
+        """Test generating image path."""
+        uuid = "test-uuid"
+        mock_uuid.return_value = uuid
+        file_path = models.recipe_image_file_path(None, "example.jpg")
+
+        self.assertEqual(file_path, f"uploads/recipe/{mock_uuid}.jpg")
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,15 +1,18 @@
 """
 Views for the recipe APIs
 """
-from core.models import Recipe, Tag, Ingredient
+from core.models import Ingredient, Recipe, Tag
 from recipe import serializers
-from rest_framework import viewsets
+from rest_framework import status, viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
     """View for managing recipe APIs."""
+
     serializer_class = serializers.RecipeDetailSerializer
     queryset = Recipe.objects.all()
     authentication_classes = [TokenAuthentication]
@@ -17,12 +20,14 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         """Retrieve recipes for authenticated user."""
-        return self.queryset.filter(user=self.request.user).order_by('-id')
+        return self.queryset.filter(user=self.request.user).order_by("-id")
 
     def get_serializer_class(self):
         """Return the serializer class for request"""
-        if self.action == 'list':
+        if self.action == "list":
             return serializers.RecipeSerializer
+        elif self.action == "upload_image":
+            return serializers.RecipeImageSerializer
 
         return self.serializer_class
 
@@ -30,23 +35,38 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Create a new recipe."""
         return serializer.save(user=self.request.user)
 
+    @action(methods=["POST"], detail=True, url_path="upload-image")
+    def upload_image(self, request, pk=None):
+        """Upload an image to recipe."""
+        recipe = self.get_object()
+        serializer = self.get_serializer(recipe, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class BaseRecipeAttrViewSet(viewsets.ModelViewSet):
     """Generic view for managing models"""
+
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        return self.queryset.filter(user=self.request.user).order_by('-name')
+        return self.queryset.filter(user=self.request.user).order_by("-name")
 
 
 class TagViewSet(BaseRecipeAttrViewSet):
     """View for managing tag APIs."""
+
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
 
 
 class IngredientViewSet(BaseRecipeAttrViewSet):
     """View for managing ingredients API."""
+
     serializer_class = serializers.IngredientSerializer
     queryset = Ingredient.objects.all()
