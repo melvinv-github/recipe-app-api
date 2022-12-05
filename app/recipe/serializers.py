b9cffe3fd6c874bd941a91375317f6db281548c7
Melvin Villas <villasmelvin@gmail.com>
2022-12-05 13:52:07 +0800

    Add ingredients from with the Recipe endpoint

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -41,12 +41,22 @@ class RecipeSerializer(serializers.ModelSerializer):
             tag_obj, _ = Tag.objects.get_or_create(user=auth_user, **tag)
             recipe.tags.add(tag_obj)
 
+    def _get_or_create_ingredients(self, ingredients, recipe):
+        """Handle getting or creating recipes as needed."""
+        auth_user = self.context['request'].user
+        for ingredient in ingredients:
+            ingredient_obj, _ = Ingredient.objects.get_or_create(
+                user=auth_user, **ingredient)
+            recipe.ingredients.add(ingredient_obj)
+
     def create(self, validated_data):
         """Create a recipe."""
         tags = validated_data.pop('tags', [])
+        ingredients = validated_data.pop('ingredients', [])
         recipe = Recipe.objects.create(**validated_data)
 
         self._get_or_create_tags(tags, recipe)
+        self._get_or_create_ingredients(ingredients, recipe)
 
         return recipe
 
