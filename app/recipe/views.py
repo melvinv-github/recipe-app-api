8b3a55150d5949bbc2d3ac849db81fdbacd105ec
Melvin Villas <villasmelvin@gmail.com>
2022-12-07 15:33:59 +0800

    For flake8

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -107,12 +107,22 @@ class BaseRecipeAttrViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        assigned_only = bool(int(self.request.query_params.get("assigned_only", 0)))
+        assigned_only = bool(
+            int(
+                self.request.query_params.get("assigned_only", 0),
+            )
+        )
         queryset = self.queryset
         if assigned_only:
             queryset = queryset.filter(recipe__isnull=False)
 
-        return queryset.filter(user=self.request.user).order_by("-name").distinct()
+        return (
+            queryset.filter(
+                user=self.request.user,
+            )
+            .order_by("-name")
+            .distinct()
+        )
 
 
 class TagViewSet(BaseRecipeAttrViewSet):
